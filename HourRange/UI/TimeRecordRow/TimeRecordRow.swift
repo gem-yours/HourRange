215d9e117ff63376d2f58c6167a346ffdd9727f3
your-gems <urgems000@gmail.com>
2023-11-02 16:40:39 +0900

    時刻が含まれるか表示するようにした

--- HourRange/UI/TimeRecordRow/TimeRecordRow.swift
+++ HourRange/UI/TimeRecordRow/TimeRecordRow.swift
@@ -12,11 +12,23 @@ class TimeRecordRow: UITableViewCell {
             startHourLabel?.text = "開始時刻: \(timeRecord?.start.value ?? 0)"
             endHourLabel?.text = "終了時刻: \(timeRecord?.end.value ?? 0)"
             hour?.text = "指定時刻: \(timeRecord?.hour.value ?? 0)"
+            backgroundColor = (timeRecord?.isContained ?? false) ? .mySystemBackground : .gray
         }
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         selectionStyle = .none
+    }
+}
+
+
+extension UIColor {
+    class var mySystemBackground: UIColor {
+        if #available(iOS 13, *) {
+            return .systemBackground
+        } else {
+            return .white
+        }
     }
 }
